8d378a16cde589f6e7b2b1fb1056764840ca7eac
hupengfei <hupengfei@kingsoft.com>
2017-08-28 15:30:22 +0800

    update:eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // function前空格
+    'space-before-function-paren': 0
   }
 };
--- demo/index.vue
+++ demo/index.vue
@@ -22,7 +22,7 @@ export default {
         return {
         };
     },
-    mounted () {
+    mounted() {
     },
     methods: {
     },
